5622e9c7f25c0eec5e2fb8a68acf4517bae0e474
Artur1509 <arturlauer1509@gmail.com>
2024-01-22 12:19:06 +0100

    Initial Commit

---
+++ JobsApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JobsApp
+//
+//  Created by Artur Lauer on 22.01.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JobsApp/JobsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobsAppApp.swift
+//  JobsApp
+//
+//  Created by Artur Lauer on 22.01.24.
+//
+
+import SwiftUI
+
+@main
+struct JobsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
