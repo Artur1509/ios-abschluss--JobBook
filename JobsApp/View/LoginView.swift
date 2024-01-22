ab652d7ff84111138fbe4ffd4736025d481cbfe4
Artur1509 <arturlauer1509@gmail.com>
2024-01-22 15:34:32 +0100

    Login View Design

--- JobsApp/View/LoginView.swift
+++ JobsApp/View/LoginView.swift
@@ -9,8 +9,67 @@ import SwiftUI
 
 struct LoginView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        Circle()
+            .fill(Color("Primary"))
+            .frame(width: 800, height: 600)
+            .padding(.top, -440)
+            .padding(.leading, -440)
+        
+        VStack {
+            Image("logo_1")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 300, height: 300)
+                
+            
+            Spacer()
+            
+            Button(action: greeting ) {
+                
+                Text("Anmelden")
+                    .frame(width: 280, height: 56)
+                    .foregroundColor(Color("Secondary"))
+                    .background(
+                        RoundedRectangle(cornerRadius: 100)
+                            .stroke(Color("Secondary"), lineWidth: 2)
+                            .background(Color.clear)
+                    )
+                
+            }
+            .padding(.bottom, 8)
+            
+            Button(action: greeting ) {
+                
+                Text("Registrieren")
+                    .frame(width: 280, height: 56)
+                    .foregroundColor(Color.white)
+                    .background(
+                        RoundedRectangle(cornerRadius: 100)
+                            .stroke(Color("Primary"), lineWidth: 2)
+                            .fill(Color("Primary"))
+                    )
+                
+            }
+            .padding(.bottom, 160)
+            
+            Spacer()
+            
+        }
+        Spacer()
+        
+        Circle()
+            .fill(Color("Secondary"))
+            .frame(width: 800, height: 600)
+            .padding(.bottom, -440)
+            .padding(.trailing, -400)
+            
+            
     }
+}
+
+func greeting() {
+    print("Test")
 }
 
 #Preview {
