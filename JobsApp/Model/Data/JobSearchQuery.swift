2ed63ffb2fda956cb7c71359bdfcf3561221c257
Artur1509 <arturlauer1509@gmail.com>
2024-02-04 09:35:56 +0100

    Filter nach beruf über Suche implementiert.

---
+++ JobsApp/Model/Data/JobSearchQuery.swift
@@ -0,0 +1,8 @@
+//
+//  JobSearchQuery.swift
+//  JobsApp
+//
+//  Created by Artur Lauer on 04.02.24.
+//
+
+import Foundation
---
+++ JobsApp/Model/SearchBar.swift
@@ -0,0 +1,18 @@
+//
+//  SearchBar.swift
+//  JobsApp
+//
+//  Created by Artur Lauer on 04.02.24.
+//
+
+import SwiftUI
+
+struct SearchBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchBar()
+}
