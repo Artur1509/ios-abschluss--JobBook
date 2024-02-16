d73cf03e9f187d7b9a80f12889a0b16b4aece236
Artur1509 <arturlauer1509@gmail.com>
2024-02-16 16:33:28 +0100

    Navigation HomeView -> Jobdetailview Logik funktioniert / Jobdetailview & den zugehörigen API Call erstellt / JobDetails Datamodel erstellt

---
+++ JobsApp/Model/Data/JobDetails.swift
@@ -0,0 +1,8 @@
+//
+//  JobDetails.swift
+//  JobsApp
+//
+//  Created by Artur Lauer on 16.02.24.
+//
+
+import Foundation
---
+++ JobsApp/View/Main/JobDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  JobDetailView.swift
+//  JobsApp
+//
+//  Created by Artur Lauer on 15.02.24.
+//
+
+import SwiftUI
+
+struct JobDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    JobDetailView()
+}
